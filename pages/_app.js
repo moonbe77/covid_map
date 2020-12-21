0558e354fce8538af59e24585a8d411dee89e2cc
Bernardo Munz <moonbe77@gmail.com>
2020-12-21 16:10:10 +1100

    added meta tags and favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import Head from 'next/head';
 import Layout from '../components/layout/Layout';
 
 const GlobalStyle = createGlobalStyle`
@@ -24,6 +25,19 @@ const theme = {
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="./covid-19.png" type="image/gif" sizes="16x16" />
+        <title>Covid-Map</title>
+        <meta
+          name="description"
+          content="This is a demo project showing the location of the recent cases
+            detected in NSW"
+        />
+        <meta name="keywords" content="HTML, CSS, JavaScript" />
+        <meta name="author" content="Bernardo Munz" />
+      </Head>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
         <Layout>
